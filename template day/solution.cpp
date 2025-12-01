5066cdef2dbabf7794c666457d1647fc4fe98edd
Siddharth Lakhara <siddharth.lakhara@gmail.com>
2025-12-01 19:41:00 +0530

    add template

---
+++ template day/solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+    public:
+    void solve() {
+
+    }
+
+    private:
+    void readInputs() {
+
+    }
+};
+
+int main() {
+  Solution solution;
+
+  // call s.solve();
+  solution.solve();
+  
+  return 0;
+}
