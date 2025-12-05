b7756d230962f8b3b308d1fa9830e559fff16025
Siddharth Lakhara <siddharth.lakhara@gmail.com>
2025-12-06 00:16:34 +0530

    day4 - code quality improvements

--- day4/day4.cpp
+++ day4/day4.cpp
@@ -1,70 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <utility> // For std::pair, std::make_pair
+#include <utility>
 
 using namespace std;
 
 class Solution {
 public:
-    void readGrid(vector<string>& grid) {
-        string line;
-        while (getline(cin, line)) {
-            grid.push_back(line);
-        }
-    }
-
-    int isPaperPresent(int rowIdx, int colIdx, vector<string>& grid) {
-        // Check bounds
-        if ((rowIdx < 0 || rowIdx >= grid.size()) || 
-            (colIdx < 0 || colIdx >= grid.at(0).size())) {
-            return 0; // Out of bounds, no paper
-        }
-        
-        // Return 1 if paper is present (not '.') or 0 if empty ('.')
-        return grid.at(rowIdx).at(colIdx) != '.';
-    }
-
-    void clearGrid(vector<pair<int, int>> &papersToRemove, vector<string>& grid) {
-        for (auto paperIdx : papersToRemove) {
-            // Set the grid cell to '.' (empty)
-            grid.at(paperIdx.first).at(paperIdx.second) = '.';
-        }
-    }
-
-    long evaluatePaperCounts(vector<string>& grid) {
-        long paperCount = 0;
-        vector<pair<int, int>> papersToRemove;
-        
-        for (int rowIdx = 0; rowIdx < grid.size(); rowIdx++) {
-            string row = grid.at(rowIdx);
-            for (int colIdx = 0; colIdx < row.size(); colIdx++) {
-                int adjPaperCount = 0;
-                
-                if (isPaperPresent(rowIdx, colIdx, grid)) {
-                    adjPaperCount = 
-                      isPaperPresent(rowIdx - 1, colIdx - 1, grid) +
-                      isPaperPresent(rowIdx - 1, colIdx, grid) +
-                      isPaperPresent(rowIdx - 1, colIdx + 1, grid) +
-                      isPaperPresent(rowIdx, colIdx - 1, grid) +
-                      isPaperPresent(rowIdx, colIdx + 1, grid) +
-                      isPaperPresent(rowIdx + 1, colIdx - 1, grid) +
-                      isPaperPresent(rowIdx + 1, colIdx, grid) +
-                      isPaperPresent(rowIdx + 1, colIdx + 1, grid);
-                    
-                    if (adjPaperCount < 4) {
-                        paperCount++;
-                        papersToRemove.push_back(make_pair(rowIdx, colIdx)); 
-                    }
-                }
-            }
-        }
-        
-        clearGrid(papersToRemove, grid);
-        
-        return paperCount;
-    }
-
     void part1() {
         vector<string> grid;
         readGrid(grid);
@@ -107,6 +49,62 @@ public:
         }
         
         cout << "Answer: " << paperCount << endl;
+    }
+
+private:
+    void readGrid(vector<string>& grid) {
+        string line;
+        while (getline(cin, line)) {
+            grid.push_back(line);
+        }
+    }
+
+    int isPaperPresent(int rowIdx, int colIdx, vector<string>& grid) {
+        if ((rowIdx < 0 || rowIdx >= grid.size()) || 
+            (colIdx < 0 || colIdx >= grid.at(0).size())) {
+            return 0;
+        }
+        
+        return grid.at(rowIdx).at(colIdx) != '.';
+    }
+
+    void clearGrid(vector<pair<int, int>> &papersToRemove, vector<string>& grid) {
+        for (auto paperIdx : papersToRemove) {
+            grid.at(paperIdx.first).at(paperIdx.second) = '.';
+        }
+    }
+
+    long evaluatePaperCounts(vector<string>& grid) {
+        long paperCount = 0;
+        vector<pair<int, int>> papersToRemove;
+        
+        for (int rowIdx = 0; rowIdx < grid.size(); rowIdx++) {
+            string row = grid.at(rowIdx);
+            for (int colIdx = 0; colIdx < row.size(); colIdx++) {
+                int adjPaperCount = 0;
+                
+                if (isPaperPresent(rowIdx, colIdx, grid)) {
+                    adjPaperCount = 
+                      isPaperPresent(rowIdx - 1, colIdx - 1, grid) +
+                      isPaperPresent(rowIdx - 1, colIdx, grid) +
+                      isPaperPresent(rowIdx - 1, colIdx + 1, grid) +
+                      isPaperPresent(rowIdx, colIdx - 1, grid) +
+                      isPaperPresent(rowIdx, colIdx + 1, grid) +
+                      isPaperPresent(rowIdx + 1, colIdx - 1, grid) +
+                      isPaperPresent(rowIdx + 1, colIdx, grid) +
+                      isPaperPresent(rowIdx + 1, colIdx + 1, grid);
+                    
+                    if (adjPaperCount < 4) {
+                        paperCount++;
+                        papersToRemove.push_back(make_pair(rowIdx, colIdx)); 
+                    }
+                }
+            }
+        }
+        
+        clearGrid(papersToRemove, grid);
+        
+        return paperCount;
     }
 };
 
