a21cbdc16e4cdcabc2e84d6dfab24c6bcf2bab50
Siddharth Lakhara <siddharth.lakhara@gmail.com>
2025-12-17 21:09:38 +0530

    add template for all days

---
+++ day9/day9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+   public:
+    void part1() {}
+
+    void part2() {}
+};
+
+int main() {
+    Solution solution;
+    solution.part1();
+    // solution.part2();
+
+    return 0;
+}
